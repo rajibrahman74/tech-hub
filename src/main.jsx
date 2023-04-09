5daad1abc9e5ceba2b2d796f2843bebfb97db905
rajibrahman74e <rajiburrahman74@gmail.com>
2023-04-10 04:49:41 +0600

    [ADD]-make-some-componets-and-import-reactDom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App'
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Layout/Main";
+import Home from "./components/Home/Home";
+import Statistics from "./components/Statistics/Statistics";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import Blog from "./components/Blog/Blog";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        paht: "tatistics",
+        element: <Statistics/>
+      },
+      {
+        path: "appliedjobs",
+        element: <AppliedJobs/>
+      },
+      {
+        path: "blog",
+        element: <Blog/>
+      }
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
